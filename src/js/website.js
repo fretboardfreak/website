ae5d7d4d6e35dfd45aa8d961c4cd42b7d4af8285
Curtis Sand <curtis@curtissand.com>
2018-12-02 16:04:20 -0800

    add very basic scss, js and html source files

---
+++ src/js/website.js
@@ -0,0 +1,15 @@
+/*
+ * website.js
+ *
+ */
+
+// functions
+
+export function load() {
+  console.log('javascript loaded.');
+}
+
+// browser events
+
+/* On DOM Ready Load the client. */
+$(document).ready(function(){load();});
